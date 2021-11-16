359281cca1bad4a818914c2a6e9968eac38f1791
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-16 11:55:15 +0100

    Feature: Added a setter method for Service so that managed property is initialized properly.

--- src/main/java/com/umer/sitemonitoring/controller/HelloController.java
+++ src/main/java/com/umer/sitemonitoring/controller/HelloController.java
@@ -1,11 +1,31 @@
 package com.umer.sitemonitoring.controller;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import com.umer.sitemonitoring.service.HelloSpringService;
 
 @ManagedBean
 public class HelloController {
 
+	// Using expression language
+	// Here, we are using Spring Bean name. By default, it is the same as the class
+	// name.
+	@ManagedProperty("#{helloSpringService}")
+	private HelloSpringService helloSpringService;
+
 	public String showHello() {
-		return "Hello from the Managed bean";
+		// return "Hello from the Managed bean";
+		return helloSpringService.sayHello();
+	}
+
+	/**
+	 * This is needed so that JSF runtime can inject the instance in the variable.
+	 * Otherwise, it will throw an exception on running the project.
+	 * 
+	 * @param helloSpringService
+	 */
+	public void setHelloSpringService(HelloSpringService helloSpringService) {
+		this.helloSpringService = helloSpringService;
 	}
 }
