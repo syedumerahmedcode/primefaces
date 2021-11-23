5389c0b4a71497fd1dc175682f908499cd25bd0e
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-23 22:29:38 +0100

    Feature: Created a Check entity.

--- src/main/java/com/umer/sitemonitoring/entity/Check.java
+++ src/main/java/com/umer/sitemonitoring/entity/Check.java
@@ -1,5 +1,23 @@
 package com.umer.sitemonitoring.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Check {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String name;
+
+	private String url;
 
 }
