7f73773642e8c52b04632af53e25942bf3e337d0
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-24 11:19:37 +0100

    Feature: Created an empty check service.

---
+++ src/main/java/com/umer/sitemonitoring/service/CheckService.java
@@ -0,0 +1,21 @@
+package com.umer.sitemonitoring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.umer.sitemonitoring.entity.Check;
+import com.umer.sitemonitoring.repository.CheckRepository;
+
+@Service
+public class CheckService {
+	
+	@Autowired
+	private CheckRepository checkRepository;
+	
+	public List<Check> findAll(){
+		return checkRepository.findAll();
+	}
+
+}
