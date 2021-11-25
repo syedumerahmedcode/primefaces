f7bc1976627e61cd8dee706861603c9409b41f96
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-25 10:25:02 +0100

    Feature: Created insert functionality in the project.

--- src/main/java/com/umer/sitemonitoring/controller/CheckListController.java
+++ src/main/java/com/umer/sitemonitoring/controller/CheckListController.java
@@ -18,24 +18,31 @@ import lombok.Setter;
 @Setter
 @ManagedBean
 @ViewScoped
-public class CheckListController implements Serializable{
-	
+public class CheckListController implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 
 	@ManagedProperty("#{checkService}")
 	private CheckService checkService;
-	
+
 	private List<Check> checks;
-	
-	private Check check=new Check();
-	
+
+	private Check check = new Check();
+
 	@PostConstruct
 	public void loadChecks() {
-		checks=checkService.findAll();
+		checks = checkService.findAll();
 	}
-	
+
 	public void save() {
 		checkService.save(check);
+		/**
+		 * Why do we need this? Because saving something creates an Id and all
+		 * subsequent calls to the name will do an update and before a select to see if
+		 * something changed.. in order ot have a new row inserted each time a save is
+		 * called, the check object must be initialized again.
+		 */
+		check = new Check();
 	}
 
 }
