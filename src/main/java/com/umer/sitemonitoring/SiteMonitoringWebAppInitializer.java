2a5d9b32638a82c68694bfe5ecd662ac6a25b0d5
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-27 09:07:20 +0100

    Feature: Code refactoring - added comments.

--- src/main/java/com/umer/sitemonitoring/SiteMonitoringWebAppInitializer.java
+++ src/main/java/com/umer/sitemonitoring/SiteMonitoringWebAppInitializer.java
@@ -7,25 +7,42 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
-
 /**
  * It bootstraps Spring in our web application
+ * 
  * @author umer
  *
  */
 public class SiteMonitoringWebAppInitializer implements WebApplicationInitializer {
 
 	public void onStartup(ServletContext container) throws ServletException {
-		
-		// Create the 'root' spring application context
-		// This is done via registering SpringCOnfiguration class which
-		// contains the annotations: @Configuration and @ComponentScan
+		AnnotationConfigWebApplicationContext rootContext = createAndRegisterRootContext();
+		addRootContextAsContextLoaderListerner(container, rootContext);
+	}
+
+	/**
+	 * Manage the life cycle of the root application context. This is achieved via
+	 * ContextLoaderListener
+	 * 
+	 * @param container
+	 * @param rootContext
+	 */
+	private void addRootContextAsContextLoaderListerner(ServletContext container,
+			AnnotationConfigWebApplicationContext rootContext) {
+		container.addListener(new ContextLoaderListener(rootContext));
+	}
+
+	/**
+	 * Create the 'root' spring application context. This is done via registering
+	 * SpringConfiguration class which contains the annotations: @Configuration
+	 * and @ComponentScan
+	 * 
+	 * @return
+	 */
+	private AnnotationConfigWebApplicationContext createAndRegisterRootContext() {
 		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
 		rootContext.register(SpringConfiguration.class);
-
-		// Manage the life cycle of the root application context
-		// This is achieved via ContextLoaderListener
-		container.addListener(new ContextLoaderListener(rootContext));
+		return rootContext;
 	}
 
 }
