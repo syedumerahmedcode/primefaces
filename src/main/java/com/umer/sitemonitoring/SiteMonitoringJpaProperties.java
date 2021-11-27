a5e77f23d469906e81d4a02b8d91d61df6dcffc8
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-27 09:33:29 +0100

    Feature: Code refactoring - extracted constants.

--- src/main/java/com/umer/sitemonitoring/SiteMonitoringJpaProperties.java
+++ src/main/java/com/umer/sitemonitoring/SiteMonitoringJpaProperties.java
@@ -4,12 +4,19 @@ import java.util.Properties;
 
 public class SiteMonitoringJpaProperties {
 
+	private static final String HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
+	private static final String HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+	private static final String JAVAX_PERSISTENCE_VALIDATION_MODE = "javax.persistence.validation.mode";
+
+	private static final String CREATE_DROP = "create-drop";
+	private static final String TRUE = "true";
+	private static final String NONE = "none";
+
 	public Properties createJpaProperties() {
 		Properties jpaProperties=new Properties();
-		jpaProperties.put("hibernate.hbm2ddl.auto", "create-drop");
-		jpaProperties.put("hibernate.show_sql", "true");
-		jpaProperties.put("javax.persistence.validation.mode", "none");
-		//jpaProperties.put("hibernate.format_sql", "true");
+		jpaProperties.put(HIBERNATE_HBM2DDL_AUTO, CREATE_DROP);
+		jpaProperties.put(HIBERNATE_SHOW_SQL, TRUE);
+		jpaProperties.put(JAVAX_PERSISTENCE_VALIDATION_MODE, NONE);
 		return jpaProperties;
 	}
 }
